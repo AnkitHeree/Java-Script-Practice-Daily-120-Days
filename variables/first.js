0a76bb588d1600907b6d745d021872d87d0a066e
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-07 17:57:08 +0530

    variables

--- variables/first.js
+++ variables/first.js
@@ -1,1 +1,11 @@
 //variables are container that is use to store values//
+//ak or cheeze java script line by line code execute krta hai //
+
+age=25;
+console.log("age is :" + age);
+
+price = 25.25;
+console.log("Abe itna he price hai lena ho to le" + price);
+
+name="Ankit";
+console.log("Ankit Here real is " + name);
