b29b037910b18dd5e5202607705afc147517a34a
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-07 18:02:15 +0530

    variables

--- variables/first.js
+++ variables/first.js
@@ -9,3 +9,9 @@ console.log("Abe itna he price hai lena ho to le" + price);
 
 name="Ankit";
 console.log("Ankit Here real is " + name);
+
+x = null;  //
+console.log("Null means pura khali nothing  " + x);
+
+undefine_wali_vcalue = undefined;
+console.log("Means Hamne koi value set nhi ki hai :--" +undefine_wali_vcalue);
