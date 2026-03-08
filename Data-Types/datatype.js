da10517c5c2a58301ce2c67fa1710f468d29a434
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-08 14:28:49 +0530

    Datatype

--- Data-Types/datatype.js
+++ Data-Types/datatype.js
@@ -20,4 +20,10 @@ console.log(ankit);
 let nuber = BigInt("100000");
 console.log(nuber)
 
+let boy = Symbol("ankit");
+console.log(boy);  // these all are primitive data types ye fix hai 
+
+// non primitive data type : object , arrays , Functions this all non primitive datatypes
+
+// object is a collaction of values like : - age name number boolean  etc  these all collecte data is called object;
 
