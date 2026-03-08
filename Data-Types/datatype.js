567f70bb8d8144a882d91076842c8bd936725026
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-08 14:23:23 +0530

    Datatype

---
+++ Data-Types/datatype.js
@@ -0,0 +1,23 @@
+// Datatypes in jas Number, string, boolean, Undefine, null, BigInt, symbol
+
+//there are two types of data types 1 Premitive :-- that is fix data type 2 NonPrimitive : -- Objects
+
+let age = 24;
+console.log(age);
+
+let name = "Ankit";
+console.log(name);
+
+let isFollow = true;
+console.log(isFollow);
+
+let people;
+console.log(people);
+
+let ankit=null;
+console.log(ankit);
+
+let nuber = BigInt("100000");
+console.log(nuber)
+
+
