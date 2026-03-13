51242b53f6bb5a8096489b4d509aa59b4146b5fe
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-13 14:50:06 +0530

    Operators

---
+++ Operators/operators.js
@@ -0,0 +1,22 @@
+
+// Arithmatic Operator
+// let a=10;
+// let b=2;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+// console.log(a%b);
+// console.log(a**b);  //exponatiation 10 ke power 2 = 100
+
+// unary Operator in javascript  1 increment++   2 decrement--
+
+let a=10;
+ let b=2;
+ console.log(a+b);
+ b--; //decrement
+ console.log(b)
+ a++;  //increment
+ console.log(a)
+
+
