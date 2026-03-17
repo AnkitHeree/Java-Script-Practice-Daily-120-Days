9be7f7c574a81f19c3964c1d1e8d55568d7d5bcd
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-17 14:22:04 +0530

    switch

---
+++ switch.js
@@ -0,0 +1,49 @@
+// switch is used when single variable having multiple conditions then we use switch
+
+// switch(expression){
+//     case value1:
+//         //code
+//         break;
+
+//         default:
+//             // code
+// }
+
+// let day=7;
+// switch(day){
+//     case 1:console.log("monday");
+//     break;
+//     case 2:console.log("tuesday");
+//     break;
+//     case 3:console.log("wednesday");
+//     break;
+//     case 4:console.log("Thrusday");
+//     break;
+//     case 5:console.log("friday");
+//     break;
+//     case 6:console.log("saturday");
+//     break;
+//     case 7:console.log("sunday");
+//     break;
+//     default:console.log(" This day dint exist")
+// }
+
+
+//when it is use jb hmare pass same variable ho and condition multiple ho tb use hoga
+
+
+// let vovel=prompt("Enter Any Vowel");
+// switch(vovel){
+//     case "a":console.log("You Entered a");
+//     break;
+//     case "e":console.log("e");
+//     break;
+//     case "i":console.log("i");
+//     break;
+//     case "o":console.log("o");
+//     break;
+//     case "u":console.log("u");
+//     break;
+   
+//     default:console.log(" not a vovel");
+// }
