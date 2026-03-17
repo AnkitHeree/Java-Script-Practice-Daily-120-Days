3ef5e8324eb6c455b06d231d0252447a905516fa
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-17 14:20:03 +0530

    array

---
+++ array.js
@@ -0,0 +1,26 @@
+// // array is one of the derived data type that is use to stre multiple values in single variables
+
+let arr = [1,2,3,4,5];  //array always closed in square bracket in side square bracket be store values same and different
+// console.log(arr[0]);  //it is a shalow copy means jo bhe hm array me change krenge o real array me bhe change hoga
+
+// const mayarr1=["ankit" ,'yadav','lol'];
+// console.log(mayarr1[1]);
+
+// // arry methods
+
+// arr.push(6)  // adding valuse in the last
+// console.log(arr)
+
+// arr.pop()  //remove last value
+// console.log(arr)
+
+// arr.unshift(9)
+// console.log(arr)  // it add startin and all value gone shift
+
+// arr.shift()
+// console.log(arr);  //it use to remove ushift value from array
+
+// console.log(arr.indexOf(4))  // use to show index value of array 
+// console.log(arr);
+
+
