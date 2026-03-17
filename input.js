038ffed902f538b1fe190435044e2d54dda3b9ee
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-17 14:20:51 +0530

    inpur

---
+++ input.js
@@ -0,0 +1,58 @@
+// let number1 = prompt("Enter any Number");
+// if(number1<=10){
+//     console.log("Answer is right");
+// }
+// else{
+//     console.log("Abe dekh kar number dall le")
+// }
+
+
+
+// let number1 = prompt("Enter Number1");
+// let number2 = prompt("Enter Number2");
+// console.log(number1+number2)   //apn ko ye 1020 ans milega because string me no adition possible
+
+
+
+// let number1 = prompt("Enter Number1");
+// let number2 = prompt("Enter Number2");
+// console.log(number1*number2)
+
+
+
+// let number1 =Number(prompt("Enter Number1"));
+// let number2 = Number(prompt("Enter Number2"));
+// console.log(number1+number2)
+
+
+
+
+// practice
+
+
+// user se 2 number lo and use bhot sare operations dikhao aur user jo operation choose kre use perform kro
+
+// let num1 = Number(prompt("Enter Mumber 1 :"));
+// let num2 = Number(prompt("Enter Mumber 2 :"));
+
+// let operation=prompt("Select any Operator : Add , sub,mul,div");
+
+
+// if(operation==="Add"){
+// console.log ("Here is your addition" + (num1+num2));
+// }
+
+// else if(operation==="sub"){
+//     console.log("Here is your addition" + (num1-num2));
+// }
+
+// else if(operation==="mul"){
+//     console.log("Here is your addition" + (num1*num2));
+// }
+
+// else if(operation==="div"){
+//     console.log("Here is your addition" + (num1/num2));
+// }
+
+
+
