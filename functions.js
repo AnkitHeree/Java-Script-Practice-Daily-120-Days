4b586d1e886d2bec7dd56a2fc10979271f7c51ff
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-18 14:46:21 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,57 @@
+//syntax
+
+// function ankit(parameter1,parameter2){
+// // code to run
+// return CSSMathValue; // kya output hoga
+
+// }
+
+
+//with parameter
+
+// ankit(agument1 , argument2)
+
+// function sumkro(a,b){
+//     let sum = a+b;
+//     return sum;
+
+// }
+// sumkro(10,20)
+
+
+//without parameters
+
+// function greet(){
+//     return "hello"
+
+// }
+// console.log(greet());
+
+
+//multiple parameters
+
+
+// function fullName(firstname,lastname){
+   
+//     return firstname+ "" + lastname;
+  
+// }
+// console.log(fullName("Ankit","Yadav"))
+
+// function add(a,b){
+//     let result=a+b;
+//     return result;  //output
+    
+    
+// }
+// console.log(add(4,5))
+
+
+function test(){
+   console.log("Hello");
+   return 0;
+}
+
+let x = test();
+console.log
+
