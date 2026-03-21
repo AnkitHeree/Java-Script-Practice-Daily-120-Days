135827d461fd8eed3335b127fbf2d25ab925cf2f
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-21 11:52:20 +0530

    functions

---
+++ function1.js
@@ -0,0 +1,62 @@
+
+// function Even(num){
+//   if(num%2==0){
+//     return "Even hai"
+//   }
+//   else{
+//    return "Odd hai"
+//   }
+// }
+
+
+// console.log(Even(25));
+
+
+
+// maximum number
+
+// function maximumNumber(max1,max2){
+// if(max1>max2){
+//     return "number one is grater Than number 2" + max1
+// }
+// else if(max2 > max1){
+//     return  max2 +"number two is grater Than number 1" 
+// }else{
+//     return "both are equal"
+// }
+// }
+// console.log(maximumNumber(20,30))
+
+
+
+//function return String Length
+
+// function stringLength(Value){
+//     return Value.length + " " + Value
+// }
+// console.log(stringLength("Ankit"));
+
+
+
+//Square of Number
+
+// function square(square){
+// let store = square * square;
+// return store;
+// }
+// console.log(square(10))
+
+
+// reverse a string
+console.log(reverse("Hello"))
+function reverse(str){
+    let result = "";
+    for(let i =4; i>=0;i--){ //
+        result+=str[i];
+    }
+
+    return result
+       
+    }
+    
+
