fce1869f87b00d200fe08285c84edd3a7c24cd59
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-25 19:17:05 +0530

    Interview Question type of functions

---
+++ types-of-functions.js
@@ -0,0 +1,50 @@
+//1 function Declaration or normal Function
+
+// in js the function is a reusable or we can say block of code co that is use to perform a specifuic task again and again when it is call
+
+// function greet(){
+//     console.log("Phle apn chlegaa munna")
+//     return "Aur ab Mai chlunga lakin mere bad koi nhi chlegaa"  // note return ke nheche ke code nhi chlte agr hmne kuch return kiya to
+//     console.log("are yar mai nhi chlunga ab return ne rok liya")
+// }
+// console.log("Mai nhi cl paa rha")  // note 2 lakin uske bahr ke code clte hai smjhee  // note 3  sbse phle ye line chlegi
+// let a=20, b=30;  
+// let add = a +b
+// console.log(add)
+// console.log(greet())  // function call niche huaa hai to ye bad me chlegaa
+
+
+
+// function greet(){  // function define 
+//     let add = 10 + 20;
+//     console.log(add)
+// }
+// greet()    // function call 
+
+
+// 2 function with parameters
+
+// function ankit(name ,age){
+//     console.log(name + " " + age )
+//     return  
+// }
+// console.log(ankit("ankit",21))   //undefine because kuch return nhi mila tujhe vhi agr 0 bhee kr de to reuton mil jata hai
+
+
+// function with return
+
+// function add(num1,num2){
+//     return num1 + num2  //return = 10 + 20 
+// }
+// let result = add(10,20)  //call
+
+// console.log(result) //
+
+// function foo() {
+//     return;
+//     {
+//         message: "Hello World";
+//     }
+// }
+
+// console.log(foo());   // undefine
