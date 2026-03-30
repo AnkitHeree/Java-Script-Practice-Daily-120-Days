8208e1c902236b9c8e83c146dacb1c68b69662e3
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-30 16:06:21 +0530

    Hoisting

--- Hoisting_Var_Function.js
+++ Hoisting_Var_Function.js
@@ -1,6 +1,37 @@
-console.log(getName);
-var x = 7
-function getName(){
-    console.log("Hello Ankit");
+// console.log(getName);
+// var x = 7
+// function getName(){
+//     console.log("Hello Ankit");
     
-}
+// }
+
+// More Concepts
+
+// console.log(a)
+//  a = 10;  // refrence error a is not define because no use of let var const
+
+
+// 2 condition
+
+// a=10;
+// console.log(a)  // 10 why because JavaScript automatically global variable bana deta hai (non-strict mode me)
+
+// 3rd condition
+
+// console.log(a)  // can not access a before initialization  Declared hai (hoisting me)                 
+// let a = 10  //  // ❌ But TDZ me hai → access allowed nahi same for const
+
+
+// {
+//   console.log(a);
+//   let a = 20;
+// }
+
+// var a = 10;
+
+// {
+//   console.log(a);
+//   let a = 20;   // note js phle apne local scope me variable dhundhta hai agr hai to vhi  use kre ka ider phir o global me jata hai 
+                      // iss case me let local scope me hai  mtlb use block me hai  is liye output 
+// }
+
