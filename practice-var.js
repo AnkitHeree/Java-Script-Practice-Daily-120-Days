3c412ac804d893aa9bef5eb5286b2695b2847e5b
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-30 16:40:50 +0530

    practice

---
+++ practice-var.js
@@ -0,0 +1,38 @@
+// console.log(ankit);
+// let ankit =10  //refrence error a ankit is not initialize
+
+// console.log(a);
+// var a = 10; // undefine
+
+
+// let ankit = 10;
+// console.log(ankit);  //10
+
+// let a = 10;
+// let b = "10";
+
+// console.log(a == b); //true
+// console.log(a === b);  //false
+
+// function test() {
+//   console.log("Hello"); //hello
+// }
+
+// test()   //hello
+// console.log(test + test()); //undefine because no return 
+
+
+// let x = 5;
+
+// function demo() {
+//   let x = 10;
+//   console.log(x);  //10
+// }
+
+// demo();
+// console.log(x);  //5
+
+
+// for(var i=0;i<3;i++){
+//     setTimeout(()=>console.log(i),4000)
+// }
