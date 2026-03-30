45733725b58e8c1cfaa383c8df9870cf89800c12
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-30 17:12:11 +0530

    practice

---
+++ practiceQuestions.js
@@ -0,0 +1,60 @@
+// Print "Hello World"
+
+// let name = "Hello World"
+// console.log(name)
+
+
+// Add two numbers
+
+// let a =10
+// let b=20
+// console.log(a+b);
+
+// let add=10+20;
+// console.log(add)
+
+
+// Find even or odd number
+
+
+
+// let value = 10;
+// if(value%2==0){
+//     console.log("Even")
+// }else{
+//     console.log("Odd")
+// }
+
+// Swap two variables
+
+// let a =10;
+// let b=20;
+// let c=a;
+// a=b;
+// b=c;
+// console.log(a,b)
+
+// option2
+
+// let a =10;
+// let b =20;
+// [a,b]=[b,a]
+// console.log(a,b)
+
+// without 3rd variable 
+
+// let a =10
+// let b =20
+// a= a+b;
+// b=a-b;
+// a=a-b;
+// console.log(a,b)
+
+// using array
+
+let a =20;
+let b=30;
+arr=[a,b];
+a=arr[1];
+b=arr[0];
+console.log(a,b)
