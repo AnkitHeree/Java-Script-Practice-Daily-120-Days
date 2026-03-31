78c56801fbf17a18443df107cd38ee5770427eb5
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-31 18:21:00 +0530

    array

--- array.js
+++ array.js
@@ -3,3 +3,10 @@
 let marks = [20,30 ,50,60]
 console.log(marks)
 console.log(marks.length)  // find length o array
+
+//hetrogenious  means  multiple dtatypes we can store in an  array 
+
+let hetrogenious = [20,"ankit",50.60,false]
+console.log(hetrogenious)
+
+console.log(hetrogenious[0])
