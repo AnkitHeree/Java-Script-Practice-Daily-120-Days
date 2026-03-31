a0c0e42d8f341e5b7c4f012c5428cda034af68f3
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-31 18:15:39 +0530

    practice

--- practiceQuestions.js
+++ practiceQuestions.js
@@ -62,9 +62,17 @@
 
 //without + and -
 
-let a =12
-let b =30
-a=a ^ b;
-b=a ^ b;
-a=a ^ b;
-console.log(a,b)
+// let a =12
+// let b =30
+// a=a ^ b;
+// b=a ^ b;
+// a=a ^ b;
+// console.log(a,b)
+
+let a =10;
+let b =20;
+let input = "";
+
+
+if(input==)
+
