ab172d8e50b408cd1b710e26c8bcf8fd62dce38a
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-31 18:42:25 +0530

    shift

--- array.js
+++ array.js
@@ -6,21 +6,26 @@
 
 //hetrogenious  means  multiple dtatypes we can store in an  array 
 
-let hetrogenious = [20,"ankit",50.60,false]
-console.log(hetrogenious)
+// let hetrogenious = [20,"ankit",50.60,false]
+// console.log(hetrogenious)
 
-// enter data in last.push
+// // enter data in last.push
 
-hetrogenious.push(90); //added back 90
-hetrogenious.push("ankit3"); // added ankit 3
-console.log(hetrogenious)
+// hetrogenious.push(90); //added back 90
+// hetrogenious.push("ankit3"); // added ankit 3
+// console.log(hetrogenious)
 
-// delet element from last
+// // delet element from last
 
-hetrogenious.pop("ankit3");
-console.log(hetrogenious)  // deleted ankit 3
+// hetrogenious.pop("ankit3");
+// console.log(hetrogenious)  // deleted ankit 3
 
-// add at  start 
+// // add at  start 
 
-hetrogenious.unshift(11)
-console.log(hetrogenious)  // 11 add to the start
+// hetrogenious.unshift(11)
+// console.log(hetrogenious)  // 11 add to the start
+
+// // for delete
+
+// hetrogenious.shift() 
+// console.log(hetrogenious)  // 11 deleted from starting  // dont pefer shift and unshift
