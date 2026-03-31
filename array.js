8717fc2576132082a9fce47a3f6e7d9b5271c32f
AnkitHeree <ankityadav9082384811@gmail.com>
2026-03-31 18:33:28 +0530

    push and pop

--- array.js
+++ array.js
@@ -1,12 +1,21 @@
 // let start array
 
-let marks = [20,30 ,50,60]
-console.log(marks)
-console.log(marks.length)  // find length o array
+// let marks = [20,30 ,50,60]
+// console.log(marks)
+// console.log(marks.length)  // find length o array
 
 //hetrogenious  means  multiple dtatypes we can store in an  array 
 
 let hetrogenious = [20,"ankit",50.60,false]
 console.log(hetrogenious)
 
-console.log(hetrogenious[0])
+// enter data in last.push
+
+hetrogenious.push(90); //added back 90
+hetrogenious.push("ankit3"); // added ankit 3
+console.log(hetrogenious)
+
+// delet element from last
+
+hetrogenious.pop("ankit3");
+console.log(hetrogenious)  // deleted ankit 3
