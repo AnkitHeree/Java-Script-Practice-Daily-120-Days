20ca5ad525d80e3acedd6835db9bb1f91cba7e53
AnkitHeree <ankityadav9082384811@gmail.com>
2026-04-10 14:25:04 +0530

    table

--- practice-loop.js
+++ practice-loop.js
@@ -11,3 +11,13 @@
 //     num++ 
 // }
 
+
+// let num = 1;
+// while(num<=20){
+//    function count(){
+//     num = num + 1;
+//     return num
+//    }
+// console.log(count())
+//     num++ 
+// }
