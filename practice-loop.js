31e52585e23f7ad7fbd922e78f23a65c3d980651
AnkitHeree <ankityadav9082384811@gmail.com>
2026-04-10 16:42:39 +0530

    reverse number

--- practice-loop.js
+++ practice-loop.js
@@ -21,3 +21,20 @@
 // console.log(count())
 //     num++ 
 // }
+
+  
+// for(i=1;i<=1000;i++){
+//     let num = i;
+//     let reverse = 0;
+
+//     while(num > 0){
+//         let lastdigit = num % 10;
+//         reverse = reverse * 10 + lastdigit
+//         num = Math.floor(num / 10)
+
+//     }
+
+//     if (i===reverse){
+//         console.log(i)
+//     }
+// }
