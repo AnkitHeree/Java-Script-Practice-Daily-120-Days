b29733781a8e3bb05e41f3cefa45290373f9b6a0
AnkitHeree <ankityadav9082384811@gmail.com>
2026-04-10 14:09:49 +0530

    loop number

--- practice-loop.js
+++ practice-loop.js
@@ -1,6 +1,13 @@
-let num = 10;
-while(num>0){
-    console.log(num)
-    num-- 
-}
+// let num = 10;
+// while(num>0){
+//     console.log(num)
+//     num-- 
+// }
+
+
+// let num = 1;
+// while(num<=10){
+//     console.log(num)
+//     num++ 
+// }
 
