f1b017dc6347ec5d06dd4965c6720eef6a52db8a
AnkitHeree <ankityadav9082384811@gmail.com>
2026-04-11 14:04:13 +0530

    Temporal Dead Zone

---
+++ TDZ/program.js
@@ -0,0 +1,20 @@
+// Q1 What is Temporal Dead Zone
+// Q2 Did Let and Const Declareration are Hoisted
+// Q3 Diffrence Between Syntax Error Refrence Error vs Type Error
+
+// Ans yes Let and Const Declaration is Hoisted but these are in the temporal dead zone and they are not in global memory they are in anpother memory and we can not access this memory space
+
+// Ex
+
+
+//  console.log(a) // we can not access a 
+// let a =10;
+// var b =20;
+
+
+// let a =10;
+// console.log(a) // now we can access it
+// var b =20;
+
+
+
