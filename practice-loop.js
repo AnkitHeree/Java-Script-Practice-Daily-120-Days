48c3f50e46709ee8914f3d180ddfc6613f00c249
AnkitHeree <ankityadav9082384811@gmail.com>
2026-04-11 14:04:50 +0530

    loops

--- practice-loop.js
+++ practice-loop.js
@@ -38,3 +38,32 @@
 //         console.log(i)
 //     }
 // }
+
+
+// for(let i = 0;i<20;i++){
+//     let num = i
+//     if(num%2==0){
+
+//         console.log("Even" , i);
+//     }
+// }
+
+// for(let i = 0;i<20;i++){
+//     let num = i
+//     if(num%2==1){
+
+//         console.log("odd" , i);
+//     }
+// }
+
+// let user = prompt("Enter a Number")
+// for(let i =1;i<user;i++){
+//     console.log(i)
+// }
+
+let table = 5;
+let num =1
+while(num<=10){
+console.log(table * num)
+num++
+}
