b03f8d3fbd4d5cfdd6ebeaa9851d0524a2539b55
AnkitHeree <ankityadav9082384811@gmail.com>
2026-04-16 14:21:18 +0530

    Most Aslk Questions everse a string

---
+++ practice-Question-That-Most-ask/revereString.js
@@ -0,0 +1,24 @@
+// Q1: Reverse a string (without using .reverse())
+
+// function revrseStr(str){
+//     let value ="";
+//     for(let i=str.length-1;i>=0;i--){
+//         value += str[i];
+//     }
+//     return value;
+// }
+// console.log(revrseStr("hello"));
+
+
+
+function reverseString(str) {
+    let arr = str.split("");
+    let result = "";
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        result += arr[i];
+    }
+
+    return result;
+}
+console.log(reverseString("hello"))
