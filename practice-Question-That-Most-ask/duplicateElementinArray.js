8fbbcb23b5c8da90c5250a71bd151cd74f9b5941
AnkitHeree <ankityadav9082384811@gmail.com>
2026-04-17 15:21:12 +0530

    Most Aslk Questions everse a string

---
+++ practice-Question-That-Most-ask/duplicateElementinArray.js
@@ -0,0 +1,10 @@
+// Q2: Find duplicate elements in array 
+
+let arr = [10,20,30,40,10,30]
+let arris = ""
+for(let i=0;i<arr.length;i++){
+    if(arr[i]== arris)
+    arris = arr[i];
+    
+    
+}
